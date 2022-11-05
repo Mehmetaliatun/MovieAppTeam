78a440944ec6a2f568c24c62fe03de96dbf8fc0b
Atun <mali1996ua@gmail.com>
2022-11-05 13:13:13 +0300

    imports in appRouter

---
+++ src/router/AppRouter.js
@@ -0,0 +1,6 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../component/navbar/Navbar";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+import Details from "../pages/details/Details";
