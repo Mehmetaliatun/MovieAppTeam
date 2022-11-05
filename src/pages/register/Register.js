2d6066d72477a533c7869500fa2dad4e96dd058d
Atun <mali1996ua@gmail.com>
2022-11-05 14:10:45 +0300

    register and login path

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,25 +1,27 @@
 import "./Register.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { Link, useLocation } from "react-router-dom";
 
 const Register = () => {
+  let path = useLocation().pathname;
   return (
     <div className="register">
       <Form className="form">
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3">
           <Form.Label>Name</Form.Label>
           <Form.Control type="text" placeholder="Enter your name" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3">
           <Form.Label>Last Name</Form.Label>
           <Form.Control type="text" placeholder="Enter your lastname" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
@@ -27,6 +29,19 @@ const Register = () => {
         <Button variant="primary" type="submit">
           Submit
         </Button>
+        <div className="warning">
+          <p className="info">
+            {path === "/login" && "Don't you have an account."}
+            {path === "/register" && "Already have an account."}
+            <Link
+              className="link"
+              to={`${path === "/login" ? "/register" : "/login"}`}
+            >
+              {path === "/register" && "Login"}
+              {path === "/login" && "Register"}
+            </Link>
+          </p>
+        </div>
       </Form>
     </div>
   );
