23b3bf012b29a72b5d0d0e79e3795cd6bebd052a
Atun <mali1996ua@gmail.com>
2022-11-05 13:40:49 +0300

    authcontext, createcontext

---
+++ src/context/AuthContext.js
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../auth/firebase-config";
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = (props) => {
+  const [currentUser, setCurrentUser] = useState();
+  onAuthStateChanged(auth, (validUser) => {
+    setCurrentUser(validUser);
+  });
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
