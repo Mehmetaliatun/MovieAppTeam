77311a54fc7ac8fa8bc11c78132174d4dd98faf2
Atun <mali1996ua@gmail.com>
2022-11-05 13:50:28 +0300

    register

--- src/auth/firebase-config.js
+++ src/auth/firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDbQyQLDL5cRONAS7ifSQQncxgFsQmQtqY",
