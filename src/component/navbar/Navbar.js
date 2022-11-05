dea64920ef4a1f31de6ddfd320a777dbba46d706
Atun <mali1996ua@gmail.com>
2022-11-05 15:21:39 +0300

    navbar firebase and auth

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,11 +1,15 @@
 import "./Navbar.css";
-import { Link, useLocation } from "react-router-dom";
-
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { auth } from "../../auth/firebase-config";
+import { AuthContext } from "../../context/AuthContext";
+import { signOut } from "firebase/auth";
+import { useContext } from "react";
 const Navbar = () => {
   let path = useLocation().pathname;
+  const navigate = useNavigate();
+  const currentUser = useContext(AuthContext);
   return (
     <div>
-      {" "}
       <div className="warning">
         <p className="info">
           {path === "/login" && "Don't you have an account"}
@@ -19,6 +23,12 @@ const Navbar = () => {
           </Link>
         </p>
       </div>
+      <Link style={{ textDecoration: "none" }} to="/">
+        <h2>
+          Movie <span>DataBase</span>
+          <h6>Go To Home Page</h6>
+        </h2>
+      </Link>
     </div>
   );
 };
