d694e6068f9f3b8a629b3ec696b57b6c4046047a
Atun <mali1996ua@gmail.com>
2022-11-05 13:24:49 +0300

    Route,Routes,Router and firebase auth

---
+++ src/auth/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbQyQLDL5cRONAS7ifSQQncxgFsQmQtqY",
+  authDomain: "movieapp-46f7d.firebaseapp.com",
+  projectId: "movieapp-46f7d",
+  storageBucket: "movieapp-46f7d.appspot.com",
+  messagingSenderId: "756936585552",
+  appId: "1:756936585552:web:5cdc5a3361db02e1d63c55",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,3 +4,19 @@ import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import Register from "../pages/register/Register";
 import Details from "../pages/details/Details";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
