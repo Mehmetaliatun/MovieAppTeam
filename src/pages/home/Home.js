4278262027c983b50d5c16ed27c0f455249db8e0
Atun <mali1996ua@gmail.com>
2022-11-05 16:06:25 +0300

    moviecard data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import { async } from "@firebase/util";
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import MovieCard from "../../component/movieCard/MovieCard";
 
+const API_KEY = `70733706aefbcc98ca02849db809a586`;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
 const Home = () => {
-  return <div>Home</div>;
+  const [movies, setMovie] = useState();
+  useEffect(() => {
+    getMovie(FEATURED_API);
+  }, []);
+  const getMovie = async (api) => {
+    try {
+      const {
+        data: { results },
+      } = await axios.get(api);
+      setMovie(results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="movieContainer">
+      {/* soru isareti optional chaining icin var yoksa 0 ceker */}
+      {movies?.map((item, i) => (
+        <MovieCard key={i} {...item} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
